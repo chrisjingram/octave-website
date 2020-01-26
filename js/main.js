d3deebb0df111a048bedae5fc11dea5d27284b58
Chris Ingram <chris@chrisingram.me>
2020-01-26 15:43:54 +0000

    Added fullscreen menu

---
+++ js/main.js
@@ -0,0 +1,8 @@
+function toggleFullscreenMenu(){
+    var el = document.getElementById("fullscreen-menu");
+    if(el.style.display === "block"){
+        el.style.display = "none"
+    }else{
+        el.style.display = "block"
+    }
+}
