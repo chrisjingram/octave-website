fd7d3c7c661b5990c196ba7c418e43615d63065e
Chris Ingram <chris@chrisingram.me>
2020-02-28 16:30:34 +0000

    Fading gallery

--- js/main.js
+++ js/main.js
@@ -1,8 +1,34 @@
 function toggleFullscreenMenu(){
-    var el = document.getElementById("fullscreen-menu");
-    if(el.style.display === "block"){
-        el.style.display = "none"
-    }else{
-        el.style.display = "block"
-    }
+  var el = document.getElementById("fullscreen-menu");
+  if(el.style.display === "block"){
+    el.style.display = "none"
+  }else{
+    el.style.display = "block"
+  }
 }
+
+// Gallery
+document.addEventListener("DOMContentLoaded", function(event){
+
+  window.currentGalleryIndex = -1;
+
+  const gallery = document.getElementsByClassName("gallery")[0];
+  const images = gallery.getElementsByTagName("img");
+
+  setTimeout(nextImage, 500);
+  setInterval(nextImage, 5000);
+
+  function nextImage(){
+    if(window.currentGalleryIndex === images.length-1){
+      window.currentGalleryIndex = 0;
+    }else{
+      window.currentGalleryIndex += 1;
+    }
+    for(var i=0; i<images.length; i++){
+      images[i].className = "";
+    }
+    images[window.currentGalleryIndex].className = "visible";
+  }
+
+});
+
